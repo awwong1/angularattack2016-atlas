d5507c57a6510ec72ac18f2d07484fd5aeef26a8
Riley Dawson <Riley Dawson>
2016-05-14 14:33:50 -0600

    changed an icon or two, added rough query builder form

--- src/app/world-data-bank/world-data-bank.component.ts
+++ src/app/world-data-bank/world-data-bank.component.ts
@@ -1,10 +1,11 @@
 import {Component} from '@angular/core';
-import {MaterializeDirective} from "angular2-materialize";
+import {MaterializeDirective} from "angular2-materialize";  
+import {QueryBuilderComponent} from "../query-builder/query-builder.component";
 
 @Component({
   selector: 'world-data-bank',
   templateUrl: 'app/world-data-bank/world-data-bank.component.html',
-  directives: [MaterializeDirective]
+  directives: [MaterializeDirective,QueryBuilderComponent]
 })
 export class WorldDataBankComponent {
   title = 'World Data Bank Component';
