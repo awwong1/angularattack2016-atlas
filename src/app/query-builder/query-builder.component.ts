4c7b172ec6f9b303fab17ea0e07519c61cc3dc6c
Alexander Wong <admin@alexander-wong.com>
2016-05-14 15:18:53 -0600

    Working Countries dropdown in the query-builder

--- src/app/query-builder/query-builder.component.ts
+++
@@ -1,8 +0,0 @@
-import { Component, Input } from '@angular/core';
-
-@Component({
-  selector: 'query-builder',
-  templateUrl: 'app/query-builder/query-builder.component.html'
-})
-export class QueryBuilderComponent {
-}
--- src/app/world-data-bank/world-data-bank.component.ts
+++ src/app/world-data-bank/world-data-bank.component.ts
@@ -1,37 +1,15 @@
 import {Component} from '@angular/core';
-import {MaterializeDirective} from "angular2-materialize";  
-import {QueryBuilderComponent} from "../query-builder/query-builder.component";
-
-import {Country} from "../shared/country";
-import {CountryService} from "../shared/country.service";
+import {QueryBuilderComponent} from "../shared/query-builder/query-builder.component";
 
 @Component({
   selector: 'world-data-bank',
   templateUrl: 'app/world-data-bank/world-data-bank.component.html',
-  directives: [MaterializeDirective,QueryBuilderComponent],
-  providers: [CountryService]
+  directives: [QueryBuilderComponent]
 })
 export class WorldDataBankComponent {
-  private static title = 'World Data Bank Component';
-  private static countryOptions:Array<Country>;
+  private title:String = '';
 
-  private countryKeys;
-  private countries;
-  private errorMessage;
-
-  constructor(private countryService:CountryService) {
-    this.countryService.getCountries()
-      .subscribe(
-        countries => {
-          console.log('subscribe:');
-          console.log(countries);
-          this.countryKeys = Object.keys(countries);
-          this.countries = countries;
-        },
-        error => {
-          console.log('error:');
-          console.log(error);
-        }
-      );
+  constructor() {
+    this.title = 'World Data Bank';
   }
 }
