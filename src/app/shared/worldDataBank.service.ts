1b8c7159797cbaf91cde00ff9f1ba034b0223942
Conner D <connerdunn7399@gmail.com>
2016-05-14 17:02:08 -0600

    Making some progress but dealing with CORS issues

--- src/app/shared/worldDataBank.service.ts
+++ src/app/shared/worldDataBank.service.ts
@@ -1,37 +1,64 @@
 import {Injectable} from '@angular/core';
-import {Http, Response} from '@angular/http';
-import {WorldDataBankResponse, DataPoint, Country, Indicator} from './country';
+import {Http, Response, Headers, RequestMethod, RequestOptions, Request} from '@angular/http';
+import {WorldDataBankResponse, DataPoint} from './worldDataBank';
+import {Country} from './country';
+import {Indicator} from './indicator';
 
 import {Observable} from 'rxjs/observable';
 
 @Injectable()
 export class WorldDataBankService {
+    public url = 'http://api.worldbank.org/countries/CAN/indicators/EG.ELC.ACCS.ZS?per_page=10&date=1960:2016&format=json';
 
   constructor(private http:Http) {
   }
+// Domain you wish to allow
+  // res.setHeader('Access-Control-Allow-Origin', 'http://localhost:3000');
+  //
+  // // Request methods you wish to allow
+  // res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE');
+  //
+  // // Request headers you wish to allow
+  // res.setHeader('Access-Control-Allow-Headers', 'YOUR-CUSTOM-HEADERS-HERE');
+  //
+  // // Set to true if you need the website to include cookies in  requests
+  // res.setHeader('Access-Control-Allow-Credentials', true);
 
-  /**
-   * Return the Promise of countries data in JSON
-   * @returns {any}
-   */
-  public getCountries():Observable<Country[]> {
-    return this.http.get(this.countriesUrl)
-      .map(this.extractData)
+  execute (): Promise<any> {
+      var tokenHeader = new Headers();
+      tokenHeader.append('Access-Control-Allow-Origin', '*');
+      tokenHeader.append('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE');
+      tokenHeader.append('Access-Control-Allow-Headers', '*');
+      var options = new RequestOptions({
+          method: RequestMethod.Get,
+          url: this.url,
+          headers: tokenHeader
+      });
+      console.log(options);
+      var req = new Request(options);
+      this.http.request(req)
+      .subscribe((res: Response) => {
+          console.log(res);
+      });
+
+      return this.http.get('/app/shared/query-resource/countries.json')
+      .toPromise()
+      .then(this.extractData)
       .catch(this.handleError);
   }
 
-  private extractData(res:Response) {
-    if (res.status < 200 || res.status >= 300) {
-      throw new Error('Response status: ' + res.status);
-    }
-    let body = res.json();
-    return body || {};
+  private extractData(res: Response) {
+      if (res.status < 200 || res.status >= 300) {
+          throw new Error('Bad response status: ' + res.status);
+      }
+      let body = res.json();
+      return body.data || { };
   }
 
-  private handleError(error:any) {
-    // In a real world app, we might use a remote logging infrastructure
-    let errMsg = error.message || 'Server error';
-    console.error(errMsg); // log to console instead
-    return Observable.throw(errMsg);
+  private handleError (error: any) {
+      // In a real world app, we might send the error to remote logging infrastructure
+      let errMsg = error.message || 'Server error';
+      console.error(errMsg); // log to console instead
+      return Promise.reject(errMsg);
   }
 }
--- src/app/world-data-bank/world-data-bank.component.ts
+++ src/app/world-data-bank/world-data-bank.component.ts
@@ -1,15 +1,18 @@
 import {Component} from '@angular/core';
 import {QueryBuilderComponent} from "../shared/query-builder/query-builder.component";
+import {WorldDataBankService} from "../shared/worldDataBank.service";
 
 @Component({
   selector: 'world-data-bank',
   templateUrl: 'app/world-data-bank/world-data-bank.component.html',
-  directives: [QueryBuilderComponent]
+  directives: [QueryBuilderComponent],
+  providers: [WorldDataBankService]
 })
 export class WorldDataBankComponent {
   private title:String = '';
 
-  constructor() {
-    this.title = 'World Data Bank';
+  constructor(worldDataBankService:WorldDataBankService) {
+      worldDataBankService.execute();
+      this.title = 'World Data Bank';
   }
 }
