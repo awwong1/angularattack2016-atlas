ededc2d6960c97bc4026e6f6d62308be2bcc4469
Alexander Wong <admin@alexander-wong.com>
2016-05-14 15:36:09 -0600

    Added indicator service

---
+++ src/app/shared/indicator.ts
@@ -0,0 +1,4 @@
+export class Indicator {
+  id:string;
+  name:string;
+}
--- src/app/shared/query-builder/query-builder.component.ts
+++ src/app/shared/query-builder/query-builder.component.ts
@@ -4,17 +4,21 @@ import {MaterializeDirective} from "angular2-materialize";
 
 import {Country} from "../country";
 import {CountryService} from "../country.service";
+import {Indicator} from "../indicator";
+import {IndicatorService} from "../indicator.service";
 
 @Component({
   selector: 'query-builder',
   templateUrl: 'app/shared/query-builder/query-builder.component.html',
   directives: [MaterializeDirective],
-  providers: [CountryService]
+  providers: [CountryService, IndicatorService]
 })
 export class QueryBuilderComponent {
   private countries:Array<Country> = [];
+  private indicators:Array<Indicator> = [];
 
-  constructor(private countryService:CountryService) {
+  constructor(private countryService:CountryService,
+              private indicatorService:IndicatorService) {
     this.countryService.getCountries()
       .subscribe(
         countries => {
@@ -25,5 +29,15 @@ export class QueryBuilderComponent {
           console.log(error);
         }
       );
+    this.indicatorService.getIndicators()
+      .subscribe(
+        indicators => {
+          this.indicators = indicators;
+        },
+        error => {
+          console.log('error:');
+          console.log(error);
+        }
+      )
   }
 }
