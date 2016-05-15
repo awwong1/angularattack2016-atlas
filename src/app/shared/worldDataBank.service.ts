bcb696cd338438fe6c3f3b7cb6a63aa01a4f1d6f
Alexander Wong <admin@alexander-wong.com>
2016-05-15 00:27:57 -0600

    fixed observable -> Observable (for surge deploy)

--- src/app/shared/worldDataBank.service.ts
+++ src/app/shared/worldDataBank.service.ts
@@ -1,7 +1,7 @@
 import {Injectable} from '@angular/core';
 import {Http, Response} from '@angular/http';
 
-import {Observable} from 'rxjs/observable';
+import {Observable} from 'rxjs/Observable';
 
 import {Country} from './country';
 import {DataPoint} from './dataPoint';
