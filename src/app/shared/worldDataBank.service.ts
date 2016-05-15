02c02d6d1accba4ca81af67bc3c753c1cfcf2b34
Alexander Wong <admin@alexander-wong.com>
2016-05-14 18:01:34 -0600

    hack for angular2 CORS wtf i dunno :poop:

--- src/app/shared/worldDataBank.service.ts
+++ src/app/shared/worldDataBank.service.ts
@@ -1,17 +1,17 @@
 import {Injectable} from '@angular/core';
 import {Http, Response, Headers, RequestMethod, RequestOptions, Request} from '@angular/http';
-import {WorldDataBankResponse, DataPoint} from './worldDataBank';
-import {Country} from './country';
-import {Indicator} from './indicator';
-
-import {Observable} from 'rxjs/observable';
 
 @Injectable()
 export class WorldDataBankService {
-    public url = 'http://api.worldbank.org/countries/CAN/indicators/EG.ELC.ACCS.ZS?per_page=10&date=1960:2016&format=json';
+
+  // we were stuck on https://github.com/angular/angular/issues/6583
+  // for about 2 hours. so now we're using this https://crossorigin.me
+  // because holy shit it is magic for CORS
+  private static corsMagic:string = 'https://crossorigin.me/';
 
   constructor(private http:Http) {
   }
+
 // Domain you wish to allow
   // res.setHeader('Access-Control-Allow-Origin', 'http://localhost:3000');
   //
@@ -24,41 +24,25 @@ export class WorldDataBankService {
   // // Set to true if you need the website to include cookies in  requests
   // res.setHeader('Access-Control-Allow-Credentials', true);
 
-  execute (): Promise<any> {
-      var tokenHeader = new Headers();
-      tokenHeader.append('Access-Control-Allow-Origin', '*');
-      tokenHeader.append('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE');
-      tokenHeader.append('Access-Control-Allow-Headers', '*');
-      var options = new RequestOptions({
-          method: RequestMethod.Get,
-          url: this.url,
-          headers: tokenHeader
-      });
-      console.log(options);
-      var req = new Request(options);
-      this.http.request(req)
-      .subscribe((res: Response) => {
-          console.log(res);
-      });
-
-      return this.http.get('/app/shared/query-resource/countries.json')
+  execute(url:string):Promise<any> {
+    return this.http.get(WorldDataBankService.corsMagic + url)
       .toPromise()
       .then(this.extractData)
       .catch(this.handleError);
   }
 
-  private extractData(res: Response) {
-      if (res.status < 200 || res.status >= 300) {
-          throw new Error('Bad response status: ' + res.status);
-      }
-      let body = res.json();
-      return body.data || { };
+  private extractData(res:Response) {
+    if (res.status < 200 || res.status >= 300) {
+      throw new Error('Bad response status: ' + res.status);
+    }
+    let body = res.json();
+    return body.data || {};
   }
 
-  private handleError (error: any) {
-      // In a real world app, we might send the error to remote logging infrastructure
-      let errMsg = error.message || 'Server error';
-      console.error(errMsg); // log to console instead
-      return Promise.reject(errMsg);
+  private handleError(error:any) {
+    // In a real world app, we might send the error to remote logging infrastructure
+    let errMsg = error.message || 'Server error';
+    console.error(errMsg); // log to console instead
+    return Promise.reject(errMsg);
   }
 }
